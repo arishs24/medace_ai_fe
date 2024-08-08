992abbd65c80bceb3d9f4e8e5bad4cdef0b93cc6
arishs24 <arish.shahab1@gmail.com>
2024-08-07 21:00:00 -0400

    phone

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
-import './index.css'
-import { AnimatedModalDemo } from './AnimatedModal'
-import { BackgroundBeams } from './ui/background-beams'
+import './index.css';
+import { AnimatedModalDemo } from './AnimatedModal';
+import { BackgroundBeams } from './ui/background-beams';
 import EmailForm from './ui/EmailForm'; // Adjust the path as necessary
 
 function App() {
   return (
     <>
-      <div className="px-4 py-5 my-5 text-center">
+      <div className="px-4 py-5 my-5 text-center container">
         <img
           className="d-block mx-auto mb-4"
           src="logo.png"
@@ -14,21 +14,22 @@ function App() {
           width={100}
           height={100}
         />
-        <h1 className="display-5 fw-bold"> Zero Anxiety, Full Confidence: Show Up Ready and Ace your Med School Interview!</h1>
+        <h1 className="display-5 fw-bold">
+          Zero Anxiety, Full Confidence: Show Up Ready and Ace your Med School Interview!
+        </h1>
         <div className="col-lg-6 mx-auto">
           <p className="lead mb-4">
             Imagine this, you’re sitting down, having a real face-to-face conversation with an AI interviewer, tailored to mimic the exact style and questions of the med school you’re aiming for. You’ll get feedback and a score, helping you improve with every session. 
             Make this a reality with MedAce: Your AI-Powered Path to Acceptance!
           </p>
           <div className="d-grid gap-2 d-sm-flex justify-content-sm-center">
-            {/* Only include the EmailForm component here */}
             <EmailForm />
           </div>
         </div>
         <BackgroundBeams />
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
